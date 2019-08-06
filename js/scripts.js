db1b16ffcd4ee4380c682257d764d1f3b5863293
Julia Martin <jmartin@a5solutions.com>
2019-08-06 12:08:05 -0300

    Previsão dos próximos dias

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
+// API UTILIZADA: https://developer.accuweather.com/apis
+
+
+// Método que busca as informações da localização digitada
 document.getElementById('lista_busca').style.display = 'none';
 const buscaAutomatica = async () => {
     var q = document.getElementById('local').value;
-    var apikey = 'oLRRYoGghTYFiU004PAtq9arMChB6GIZ'; 
+    var apikey = 'FRA4sNMyyDbvG5V8gQZISDifa3NuUszv'; 
     const response = await fetch('http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=' + apikey + '&q=' + q + '&language=pt-br');
     const myJson = await response.json();
     
@@ -24,13 +28,11 @@ const buscaAutomatica = async () => {
 document.getElementById('search-button').onclick = buscaAutomatica;
 
 
-
-
-
+// Método que atualiza as informações principais do dia
 const atualizaInfo = async () => {
     var id = localStorage.getItem('key');
     console.log('Local: ' + id);
-    var apikey = 'oLRRYoGghTYFiU004PAtq9arMChB6GIZ'; 
+    var apikey = 'FRA4sNMyyDbvG5V8gQZISDifa3NuUszv'; 
     const response = await fetch('http://dataservice.accuweather.com/currentconditions/v1/' + id +'?apikey=' + apikey +'&language=pt-br');
     const myJson = await response.json();
 
@@ -44,23 +46,24 @@ const atualizaInfo = async () => {
 }
 
 
-
-
-
+// Método que atualiza as informações dos 4 próximos dias
 const atualizaDias = async () => {
   var id = localStorage.getItem('key');
-  var apikey = 'oLRRYoGghTYFiU004PAtq9arMChB6GIZ'; 
+  var apikey = 'FRA4sNMyyDbvG5V8gQZISDifa3NuUszv'; 
   const response = await fetch('http://dataservice.accuweather.com/forecasts/v1/daily/5day/' + id + '?apikey='+apikey+'&language=pt-br&metric=true');
   const myJson = await response.json();
 
   var semana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
   var data = new Date();
   var noite = false;
+
+  //Verifica o horário para ver se é noite ou não
   if(data.getHours() < 5 && data.getHours() > 19){
     noite = true;
   }else{
     noite = false;
-  }
+  }  
+
   console.log('Noite: ' + noite + ' | Hora atual: ' + data.getHours());
 
   //HOJE
@@ -69,27 +72,38 @@ const atualizaDias = async () => {
   document.getElementById('texto_max_min').innerHTML = min_hoje + '°C / ' + max_hoje + '°C';
 
   //PRÓXIMOS DIAS
-  var min_1 = myJson.DailyForecasts[1].Temperature.Minimum.Value;
-  var max_1 = myJson.DailyForecasts[1].Temperature.Maximum.Value;
-  if(noite){
-    var id = trataIcone(myJson.DailyForecasts[1].Night.Icon);
-    var icone_semana_1 = "url('https://developer.accuweather.com/sites/default/files/"+id+"-s.png')";
-  }else{
-    var id = trataIcone(myJson.DailyForecasts[1].Day.Icon);
-    var icone_semana_1 = "url('https://developer.accuweather.com/sites/default/files/"+id+"-s.png')";
-  }
   var dias = document.getElementsByClassName("dayname");
+  var temperatura_dias = document.getElementsByClassName("max_min_temp");
+  var icone_semanas = document.getElementsByClassName("daily_weather_icon");
   
   for(var i = 0; i < dias.length; i++){
+    // Trata o dia da semana
       var data_semana = new Date(myJson.DailyForecasts[i].Date);
       dias[i].innerHTML = semana[data_semana.getDay()];
-      console.log(semana[data_semana.getDay()]);
+      console.log('Dia da semana: ' + semana[data_semana.getDay()]);
+  
+    // Trata a temperatura mínima e máxima 
+      var min_previsao = myJson.DailyForecasts[i].Temperature.Minimum.Value;
+      var max_previsao = myJson.DailyForecasts[i].Temperature.Maximum.Value;
+      temperatura_dias[i+1].innerHTML = min_previsao + '°C / ' + max_previsao + '°C';
+      console.log(min_previsao + '°C / ' + max_previsao + '°C');
+
+    // Trata os icones
+    if(noite){
+      var id_icon = trataIcone(myJson.DailyForecasts[i].Night.Icon);
+      var icone_semana_1 = "url('https://developer.accuweather.com/sites/default/files/"+id_icon+"-s.png')";
+    }else{
+      var id_icon = trataIcone(myJson.DailyForecasts[i].Day.Icon);
+      var icone_semana_1 = "url('https://developer.accuweather.com/sites/default/files/"+id_icon+"-s.png')";
+    }
+
+    icone_semanas[i].style.backgroundImage = icone_semana_1;
+
   }
 
 }
 
-
-
+// MÉTODOS UTILITÁRIOS
 // Método que adiciona o 0 antes de números menores que 10
 function trataIcone(idIcone){
   var resposta;
@@ -100,13 +114,4 @@ function trataIcone(idIcone){
   }
   return resposta;
 }
-
-
-
-
-/* APIS
- clima, previsão 12 horas e previsão 5 dias: https://developer.accuweather.com/apis
- pegar coordenadas geográficas pelo nome da cidade: https://docs.mapbox.com/api/
- pegar coordenadas do IP: http://www.geoplugin.net
- gerar gráficos em JS: https://www.highcharts.com/demo
-*/
+//===============================================================================================
